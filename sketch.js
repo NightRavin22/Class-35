f527bb9c2e53fd4bbeb0c2f7ab32031d319fcfd5
NightRavin22 <73198686+NightRavin22@users.noreply.github.com>
2021-01-05 15:57:58 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var hyponticBall;
+var database,position;
+var gameState=0;
+var playerCount,form,player,game;
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+    game=new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    background("white");
+    
+    drawSprites();
+}
